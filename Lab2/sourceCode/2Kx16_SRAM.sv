bccf1e1b9a83312ff686e3bc12536c67b1d06b9a
rmui34 <r.mui34@gmail.com>
2015-04-18 17:24:05 -0700

    SRAM skel

---
+++ Lab2/sourceCode/2Kx16_SRAM.sv
@@ -0,0 +1,17 @@
+module 2Kx16_SRAM(adx,chpSel,OutEn,WrEn,DataIn,DataOut);
+input [11:0] adx;
+input chpSel;
+input OutEn;
+input WrEn;
+input [15:0] DataIn;
+output [15:0] DataOut;
+
+
+
+
+
+
+
+
+
+endmodule
