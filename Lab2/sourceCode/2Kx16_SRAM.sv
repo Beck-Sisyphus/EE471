a9fff564dcdf2674393d76f220700be5eabfa682
rmui34 <r.mui34@gmail.com>
2015-04-19 22:11:25 -0700

    some changes to sram

--- Lab2/sourceCode/2Kx16_SRAM.sv
+++ Lab2/sourceCode/2Kx16_SRAM.sv
@@ -15,21 +15,21 @@ module 2Kx16_SRAM(clk,adx,chpSel,OutEn,WrEn,data);
 	// clock
 	decoder8_256 decoder(adx[10:3],indices);
 	
-	mux8_1 mux15 (out[15],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux14 (out[14],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux13 (out[13],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux12 (out[12],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux11 (out[11],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux10 (out[10],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);	
-	mux8_1 mux9 (out[9],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux8 (out[8],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux7 (out[7],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);	
-	mux8_1 mux6 (out[6],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux5 (out[5],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux4 (out[4],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);	
-	mux8_1 mux3 (out[3],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);	
-	mux8_1 mux2 (out[2],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux1 (out[1],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
-	mux8_1 mux15 (out[0],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux15 (data[15],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux14 (data[14],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux13 (data[13],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux12 (data[12],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux11 (data[11],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux10 (data[10],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);	
+	mux8_1 mux9 (data[9],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux8 (data[8],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux7 (data[7],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);	
+	mux8_1 mux6 (data[6],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux5 (data[5],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux4 (data[4],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);	
+	mux8_1 mux3 (data[3],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);	
+	mux8_1 mux2 (data[2],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux1 (data[1],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
+	mux8_1 mux0 (data[0],in0,in1,in2,in3,in4,in5,in6,in7,adx[2],adx[1],adx[0]);
 	
 endmodule
